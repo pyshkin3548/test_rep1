8f0248e0fb6a2cc4d5939e81b123b949e6eb58f4
pyshkin3548 <63065894+pyshkin3548@users.noreply.github.com>
2022-08-09 16:01:57 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import calendar
+
+print('Добро пожаловать в наш супер-пупер календарь\n')
+
+year = int(input('Пожлуйста, введите год: '))
+month = int(input('Введите номер любого месяца: '))
+
+print(calendar.month(year, month))
+
+print('Всего хорошего!!!')
